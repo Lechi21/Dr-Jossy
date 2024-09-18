028792be3f82e44b7a4abd9c0c249b3cf458fbd4
Kelechi Gift <112638489+Lechi21@users.noreply.github.com>
2024-09-18 16:24:35 +0100

    Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+function showMenu() {
+    document.getElementById("nav").style.right = "0";
+}
+
+function hideMenu() {
+    document.getElementById("nav").style.right = "-100%";
+}
+
+
+function home() {
+    window.scrollTo(0, 0);
+}
